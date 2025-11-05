8a646aeda92f4b2ce97308fb45d3b9095c958397
Matheus <152727396+MatheusMarinhoLopes@users.noreply.github.com>
2025-11-05 22:55:11 +0000

    Termino do Desafio Nível Novato do jogo de Xadrez

--- xadrez.c
+++ xadrez.c
@@ -11,11 +11,39 @@ int main() {
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+    int bispo = 0; // variável da peça Bispo
+
+    printf("Movimento do Bispo (5 casas na diagonal superior direita): \n"); // diz qual é o movimento da peça do Bispo
+
+    while (bispo < 5){
+        printf(" Diagonal Superior Direita \n"); // imprime a direção do movimento do Bispo para diagonal superior direita
+        bispo++;
+    }
+        printf("\n"); // comando para pular linha
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    printf("Movimento da Torre (5 casas para a direita): \n"); // diz qual é o movimento da peça da Torre
+
+    for (int torre = 0; torre < 5; torre++){  // a variável Torre está aqui dentro
+        printf(" Direita \n");  // imprime a direção do movimento da Torre para direita
+    }
+        printf("\n"); // comando para pular linha
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+    int rainha = 0; // variável da peça Rainha
+
+    printf("Movimento da Rainha (8 casas para a esquerda): \n"); // diz o movimento da peça da Rainha
+
+    do {
+        printf(" Esquerda \n"); // imprime a direção do movimento da Rainha para esquerda
+        rainha++;
+    } while (rainha < 8);
+
+        printf("\n"); // comando para pular linha
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
